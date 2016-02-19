ee2dc7084f95cf3e20cf7406b39d836ad3f65bef
danandjulia <appacademy@Apps-Mac-mini-166.local>
2016-02-19 16:35:54 -0500

    Created LinkedList class

--- lib/p03_hash_set.rb
+++ lib/p03_hash_set.rb
@@ -9,12 +9,21 @@ class HashSet
   end
 
   def insert(key)
+    resize! if count >= num_buckets
+    bucket = key.hash % num_buckets
+    @store[bucket] << key
+    @count += 1
   end
 
   def include?(key)
+    bucket = key.hash % num_buckets
+    @store[bucket].include?(key)
   end
 
   def remove(key)
+    bucket = key.hash % num_buckets
+    @store[bucket].delete(key)
+    @count -= 1
   end
 
   private
@@ -28,5 +37,13 @@ class HashSet
   end
 
   def resize!
+    new_array = Array.new(num_buckets * 2) { Array.new }
+
+    temp_array = @store.flatten
+    @store = new_array
+    @count = 0
+    temp_array.each do |val|
+      self.insert(val)
+    end
   end
 end
--- lib/p04_linked_list.rb
+++ lib/p04_linked_list.rb
@@ -1,4 +1,5 @@
 class Link
+
   attr_accessor :key, :val, :next, :prev
 
   def initialize(key = nil, val = nil)
@@ -14,7 +15,11 @@ class Link
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head = nil
+    @tail = nil
   end
 
   def [](i)
@@ -23,27 +28,74 @@ class LinkedList
   end
 
   def first
+    @head
   end
 
   def last
+    @tail
   end
 
   def empty?
+    @head.nil?
   end
 
   def get(key)
+    return first unless first
+    value = first.key
+    node = first
+    until value == key
+      node = node.next
+      return nil unless node
+      value = node.key
+    end
+    node.val
   end
 
   def include?(key)
+    included = false
+    included = true if get(key)
+
+    included
   end
 
+  # def initialize(key = nil, val = nil)
+  #   @key = key
+  #   @val = val
+  #   @next = nil
+  #   @prev = nil
+  # end
   def insert(key, val)
+    new_link = Link.new(key, val)
+    new_link.prev = last
+    if self.first
+      new_link.prev.next = new_link
+    else
+      @head = new_link
+    end
+    @tail = new_link
   end
 
   def remove(key)
+    return first unless first
+    value = first.key
+    node = first
+    until value == key
+      node = node.next
+      return nil unless node
+      value = node.key
+    end
+    node.prev.nil? ? @head = node.next : node.prev.next = node.next
+    node.next.nil? ? @tail = node.prev : node.next.prev = node.prev
   end
 
-  def each
+  def each(&prc)
+    prc ||= Proc.new { |val| val }
+    node = first
+    until node.nil?
+      prc.call(node)
+      node = node.next
+    end
+
   end
 
   # uncomment when you have `each` working and `Enumerable` included
